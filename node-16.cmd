ff1d660bb7056cbd9c311a016af156f683859ce5
CharlesVG <charles.vangoidsenhoven@vdab.be>
2021-12-20 10:06:41 +0100

    Initial commit

---
+++ node-16.cmd
@@ -0,0 +1,9 @@
+@echo off
+set batchPath=%~dp0
+powershell.exe -file "%batchPath%node-16.ps1"
+call refreshenv.cmd
+
+SET ACTUAL_VERSION=
+FOR /F %%I IN ('node -v') DO @SET "ACTUAL_VERSION=%%I"
+
+@echo Now using node %ACTUAL_VERSION%
